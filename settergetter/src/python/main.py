4bcf8627a43b074f023aa0373fb0ab0a7e38168a
Bagus Nurtomo <bagusthanatos@gmail.com>
2018-07-29 14:16:15 +0700

    Python version should now be capable of detecting VERY simple ones(with mistakes)

--- settergetter/src/python/main.py
+++ settergetter/src/python/main.py
@@ -1,7 +1,34 @@
 import sys
 import os
+import datetime
+
+
+def containsPublicSetterGetter(filename):
+	detected = False
+	with open(filename, 'rt') as f:
+		for line in f :
+			tokens = line.strip().split()
+			if len(tokens) < 3:
+				detected = False
+				continue
+			if detected and ((tokens[0].startswith("this.") and tokens[1] == '=') or tokens[0] == 'return'):
+				print(line)
+				return True
+			else:
+				detected = False
+
+			if tokens[0] == 'public':
+				if tokens[1] == 'void' and tokens[2].startswith('set'):
+					detected = True
+					continue
+				if tokens[2].startswith('get'):
+					print(line)
+					detected = True
+	return False		
+			
 
 if __name__ == "__main__":
+	start = datetime.datetime.now()
 	print(sys.argv)
 	if len(sys.argv) < 2:
 		print("Please set the zeppelin directory")
@@ -10,23 +37,21 @@ if __name__ == "__main__":
 	if not os.path.isdir(path):
 		print("%s is not a valid directory" %path)
 	
-	f = open("E:/KMK/zeppelin/zeppelin-zengine/src/main/java/org/apache/zeppelin/notebook/Note.java", "rt")
-	detected = False
-	for line in f :
-		tokens = line.strip().split()
-		if len(tokens) < 3:
-			detected = False
-			continue
-		if detected and (tokens[0].startswith("this.") or tokens[0] == 'return'):
-			print(line)
-			detected = False
-		if tokens[0] == 'public':
-			if tokens[1] == 'void' and tokens[2].startswith('set'):
-				print(line)	
-				detected = True
-				continue
-			if tokens[2].startswith('get'):
-				print(line)
-				detected = True
-			
-	f.close()
+	count = 0
+	totalfiles = 0
+	for dirname, dirnames, filenames in os.walk(path):
+#		print(dirnames)
+#		print(dirname)
+#		print(filenames)
+		if '.git' in dirnames:
+			dirnames.remove('.git')
+		for filename in filenames:
+			if 'java' in filename:
+				totalfiles+=1
+				if  containsPublicSetterGetter(os.path.join(dirname, filename)):
+					#print(filename)
+					count+=1
+	time = datetime.datetime.now() - start
+	print("Time spent: %s", str(time))
+	print("Java Files with public setter/getter: %s", count)
+	print("Java Files : %s", totalfiles)
